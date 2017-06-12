aadc84b26b57fdf47ff31d309326af0d7f1c5935
João Paulo Rods <joaopaulorods@gmail.com>
2017-06-12 15:33:54 -0300

    Mais um commit teste

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="pt-br"> 
 <head>
 <meta charset="utf-8">
-<title>Curso Git</title> 
+<title>Curso Git e Github</title> 
 </head> 
 <body>
-	<p>Testando alguns comandos!</p>
+	<p>Testando alguns comandos do curso Curso Git e Github!</p>
 </body>
 </hmtl>
